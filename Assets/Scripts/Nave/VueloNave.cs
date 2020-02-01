c51367e8b79af05a38ac15e2fd77f343fe23d29b
torcu <pablotorcuato@gmail.com>
2020-02-01 21:50:52 +0100

    Despegue y Vuelo

---
+++ Assets/Scripts/Nave/VueloNave.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VueloNave : MonoBehaviour
+{
+    public float velocidad;
+    public bool volando = false;
+    private void FixedUpdate()
+    {
+        if (volando)
+        {
+            transform.Translate((Vector3.up * velocidad)*Time.deltaTime);
+        }
+    }
+    public void Despega()
+    {
+        volando = true;
+        GetComponent<Animator>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/Rotador.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotador : MonoBehaviour
+{
+
+    public Vector3 direccionRotacion;
+    float multiplicador;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time <= 5)
+        {
+            multiplicador = +Time.time;
+        }
+        transform.Rotate(direccionRotacion * multiplicador * Time.deltaTime);
+    }
+}
