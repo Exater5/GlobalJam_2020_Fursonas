a300232994e25adc320eea922ba3f166516314fa
lszlo <33127492+lszlo@users.noreply.github.com>
2020-02-01 12:21:11 +0100

    Movimiento

---
+++ Assets/Scripts/Navigation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigation : MonoBehaviour
+{
+    [SerializeField]
+    public List<Vector3> navPoints;
+
+    public void Awake()
+    {
+        navPoints = new List<Vector3>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            navPoints.Add(transform.GetChild(i).position);
+        }
+    }
+}
