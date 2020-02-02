ad16be0b03a1f4d1e21a07c96575db2fff35cff9
torcu <pablotorcuato@gmail.com>
2020-02-02 12:46:13 +0100

    Update Materials

---
+++ Assets/Scripts/CambiaCamaras.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambiaCamaras : MonoBehaviour
+{
+    Camera cPrincipal;
+    Camera cMinijuego;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cPrincipal = Camera.main;
+    }
+
+    public void CambiaCamara()
+    {
+        cPrincipal.enabled = false;
+    }
+    public void ReactivaPrincipal()
+    {
+        cPrincipal.enabled = true;
+    }
+}
