7e2f61b0f05b187623b834642643c92dc0ddb2b4
torcu <pablotorcuato@gmail.com>
2020-02-02 11:55:42 +0100

    Menu settings

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     bool pausado = false;
     public Animator animMenuIngame;
+    public Image botonPausa;
+    public Sprite[] pausa;
     public void Pausa()
     {
         if (!pausado)
@@ -14,12 +16,14 @@ public class GameController : MonoBehaviour
             Time.timeScale = 0f;
             animMenuIngame.SetTrigger("Entra");
             pausado = true;
+            botonPausa.sprite = pausa[1];
         }
         else
         {
             Time.timeScale = 1;
             animMenuIngame.SetTrigger("Sale");
             pausado = false;
+            botonPausa.sprite = pausa[0];
         }
     }
     public void Reinicia()
